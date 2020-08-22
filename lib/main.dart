5bc5f0596e0ced2357f7632f873a64e0f46b702b
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-08-22 21:55:04 +0900

    アイコンをタップ可能に

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _saved =  Set<WordPair>();
+  final _saved = Set<WordPair>();
   final _biggerFont = TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
@@ -46,11 +46,17 @@ class _RandomWordsState extends State<RandomWords> {
   Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return ListTile(
-      title: Text(pair.asPascalCase, style: _biggerFont),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite :  Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null
-      ),
-    );
+        title: Text(pair.asPascalCase, style: _biggerFont),
+        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        });
   }
 }
